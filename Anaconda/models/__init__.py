0fe2ceb4dfa1c9d3311275e5e8e3a618441f9d73
VinulaUthsara <vinula9@gmail.com>
2021-04-06 14:13:17 +0530

    added all other files to anaconda directory

---
+++ Anaconda/models/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import models.autogan_cifar10_a
+import models.autogan_cifar10_b
+import models.autogan_cifar10_c
